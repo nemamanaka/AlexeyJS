8c53219c73a07c3282191cd6f8fa1176c7d81c92
nemamanaka <invise.corn@gmail.com>
2023-07-14 18:21:18 +0300

    Homework3
    Homework3

---
+++ homework3/js/main.js
@@ -0,0 +1,87 @@
+
+// 1.
+
+//  let askName = prompt('Как вас зовут?: ');
+
+// alert('Привіт ' + askName); 
+
+
+
+/////////////////////////////////////////////////
+
+
+// 2
+
+//  let str = prompt("Пожалуйста вставте рядок: ");
+
+// let modString = str.replace(/,/, ", ");
+
+// console.log(modString); 
+
+
+////////////////////////////////////////////////
+
+
+// 3
+
+//  let String = "telePHONe";
+// let result = String.charAt(0).toUpperCase() + String.slice(1).toLowerCase();
+
+// console.log(result); 
+
+
+////////////////////////////////////////////////
+
+
+// 4
+
+// let str = 'Сколько слов в этом рядке';
+
+// let words = str.split(" ");
+
+// let wordCount = words.length;
+
+// console.log(wordCount); 
+
+
+//////////////////////////////////////////////////
+
+
+// 5
+
+//  let lastName = prompt('Введите вашу фамилию: ');
+// let firstName = prompt('Введите ваше имя: ');
+// let middleName = prompt('Введите ваше отчество: ');
+
+// lastName = lastName.trim();
+// firstName = firstName.trim();
+// middleName = middleName.trim();
+
+// firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+// lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+// middleName = middleName.charAt(0).toUpperCase() + middleName.slice(1).toLowerCase();
+
+
+// let fullName = lastName + " " + firstName + " " + middleName;
+
+// console.log(fullName); 
+
+
+///////////////////////////////////////////////////////
+
+
+// 6
+
+// let str = "Было пасмурно, солнце светило ярко";
+// let result = str.split("пасмурно").join("солнечно");
+
+// console.log(result); 
+
+
+///////////////////////////////////////////////////////
+
+
+ 
+	
+	
+	
